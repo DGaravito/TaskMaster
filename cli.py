9083e49822e9643d0f72770c58839fe268201d7f
David Garavito <d.garavito2@gmail.com>
2022-07-22 11:54:20 -0400

    commented cli.py

--- cli.py
+++ cli.py
@@ -1,14 +1,18 @@
 from Guis.main import main
 import logging
 
+# anything will get logged in a log.txt file in the application's directory
 logging.basicConfig(filename='log.txt', level=logging.DEBUG)
 
+# main loop
 if __name__ == '__main__':
 
+    # run the main python gui
     try:
 
         main()
 
+    # in case of error, log it in the txt
     except Exception:
 
         logging.error('A critical error occured', exc_info=True)
